0e90ebae4883473a9fffefaf0f669511eec06ca5
gnanu27 <gnanavel.py2k@gmail.com>
2019-12-11 10:32:24 +0530

    bubble-sort-2

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,13 +1,17 @@
 def bubbleSort(A):
-    length = len(A)
-    for i in range(1, len(A)):
-        pos = 1
-        while pos < length:
-            if A[pos] < A[pos - 1]:
-                A[pos], A[pos-1] = A[pos - 1], A[pos]
-            pos += 1
-        length -= 1
+    # length = len(A)
+    # for i in range(1, len(A)):
+    #     pos = 1
+    #     while pos < length:
+    #         if A[pos] < A[pos - 1]:
+    #             A[pos], A[pos-1] = A[pos - 1], A[pos]
+    #         pos += 1
+    #     length -= 1
 
+    for i in range(len(A)-1, 0, -1):
+        for j in range(i):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
 
 
 A = [84, 21, 96, 15, 47]
